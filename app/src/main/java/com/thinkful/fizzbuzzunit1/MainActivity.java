460a9f8c650edad7c56bf68832a6bb50062a1ecb
Diego Villalta <diegovillalta@Diegos-MacBook-Pro.local>
2015-02-01 14:21:39 -0600

    Initial commit

---
+++ app/src/main/java/com/thinkful/fizzbuzzunit1/MainActivity.java
@@ -0,0 +1,103 @@
+package com.thinkful.fizzbuzzunit1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View;
+
+
+public class MainActivity extends Activity {
+
+    TextView textView;
+    EditText editText;
+    String fizzString = "Fizz";
+    String buzzString = "Buzz";
+    String fizzBuzzString = "FizzBuzz";
+    String valueString;
+
+
+    public String checkValue(int i){
+
+        //divisible por 3 y por 5
+        if (i % 3 == 0 && i % 5 == 0){
+
+           valueString = fizzBuzzString;
+
+        }
+        // divisible por 3
+        else if (i%3 == 0) {
+
+            valueString = fizzString;
+        }
+        // divisible por 5
+        else if (i%5 == 0) {
+
+            valueString = buzzString;
+        }
+        else{
+            valueString = String.valueOf(i);
+        }
+
+            return valueString;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void doButton(View view){
+        // Implement the function of the button here
+        // Use numberOfValues, and create a string array of both the numbers and words,
+        // replacing the appropriate numbers with the words fizz or buzz
+
+        editText = (EditText) findViewById(R.id.editText); // user input
+        textView = (TextView) findViewById(R.id.fizzText); // where the text will display
+        textView.setText("" + "\n");
+
+
+        int numberOfValues = Integer.parseInt(editText.getText().toString());
+
+        for (int i=1; i <= numberOfValues;i++){
+
+
+
+
+
+            textView.append(checkValue(i) + "\n");
+
+        }
+        
+
+    }
+}
